bb7359add567dcbf6f22cc1e6bc36178520fa2f8
Rasheem Khan <rasheemkhan@rasheems-mbp.mynetworksettings.com>
2023-01-22 17:07:40 -0500

    sigh

---
+++ PracticeHW/practiceHW.js
@@ -0,0 +1,113 @@
+//Array examples 
+let myMantra = ["eat","sleep", "repeat"];
+let numArray = [1,2,3]
+let dogArray = ["shiba","dalamatian","golden retriever"]
+let num20s = [20,40,60,80,100]
+let sneakerArray = ["jordan","adidas","nike"]
+let metalArray = ["gold","silver","bronze"]
+let tvShowsArray =["nickelodeon","disney","cartoon Network"]
+let gameSystems =["gameboy","playstation","xbox"]
+let storeNames = ["nordstorm","fashionnova","steve madden"]
+let vegetablesArray= ["broccoli","cauliflower","tomatoes"]
+console.log(myMantra)
+console.log(numArray)
+console.log(dogArray)
+console.log(num20s)
+console.log(sneakerArray)
+console.log(metalArray)
+console.log(tvShowsArray)
+console.log(gameSystems)
+console.log(storeNames)
+console.log(vegetablesArray)
+
+console.log(myMantra[2], num20s[3])
+// arrays always start at 0//
+// when targeting a specfic item in an array, it always starts at 0//
+
+// String examples
+let cereal = "Fruit Loops"
+let cereal1 = "Raisin Bran"
+let cereal2 = "Frosted Flakes"
+let cereal3 = "Corn flakes"
+let cereal4 = "Cinnamon Toast Crunch"
+let cereal5 = "Fruity Pebbles"
+let cereal6 = "Cocoa Pebbles"
+let cereal7 = "Coco Puffs"
+let cereal8 = "Apple Jacks"
+let cereal9 = "Captain Crunch"
+
+//Interger Examples
+let num = 101
+let num1 = 201
+let num2 = 301
+let num3 = 401
+let num4 = 501
+let num5 = 601
+let num6 = 701
+let num7 = 801
+let num8 = 901
+let num9 = 1001
+
+//Object examples
+
+let car = {
+    mileage: 30000,
+    type: "infiniti",
+    year: 2019
+  };
+  
+  let iceCream = {
+    brand: "Haggendaz",
+    flavor: "cookies & cream",
+    size: "pint"
+  };
+
+let weather = {
+    temp: "70 degrees",
+    status: "drizzle" ,
+    humidity: "66 percent"
+}
+
+let breakfast = {
+    food: "waffles",
+    drink: "tea",
+    fruit: "bananas"
+}
+
+let person = { 
+    firstName: "whatsIt",
+    lastName: "toYuh?",
+    race: "fish"
+}
+
+let clothes = {
+    color: "teal",
+    type: "turtleneck",
+    fabric: "cashmere"
+}
+
+let flowers = {
+    type: "orchids",
+    color: "purple",
+    price: 354
+}
+ 
+let phone = {
+    brand: "iPhone",
+    model: 11,
+    color: "white",
+}
+
+let cat = {
+    breed: "tabby",
+    name: "OJ",
+    color: "gray"
+}
+
+let anime = {
+    genre: "shonen",
+    name: "Attack on Titan",
+    episodes: "ongoing"
+
+}
+

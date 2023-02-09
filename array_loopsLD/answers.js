5294675a2fb64c6d17b4718a3f7c051907819b11
Akili Inniss <rasheemkhan@rasheems-mbp.mynetworksettings.com>
2023-02-09 13:50:07 -0500

    Easy Going Answered

---
+++ array_loopsLD/answers.js
@@ -0,0 +1,39 @@
+1.////////////////////////////////
+
+for(let i = 1; i <= 20; i++) 
+  console.log(i);
+////////////////////////////////
+2.// 
+for(let i = 0; i <= 200; i++) 
+ if (i % 2 ===0){
+  console.log(i)
+ }
+
+3.////////////////////////////////
+for(let i = 1; i <= 100; i++) 
+ if (i % 3 ===0){
+  console.log("Fizz")
+ } else if(i % 5 ===0){
+    console.log("Buzz")
+} else if (i % 3 === 0  && i % 5 === 0){
+    console.log("FizzBuzz")
+} else {
+    console.log(i)
+}
+
+4.// ////////////////////////////////
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+plantee.splice(2)
+console.log(plantee)
+////////////////////////////////
+// Fizz Buzz
+////////////////////////////////
+
+
+////////////////////////////////
+// Wild Wild Life
+////////////////////////////////
